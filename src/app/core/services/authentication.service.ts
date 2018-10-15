ff1a357c12bed0a3c76309fa6e0048515c39a20d
António Felgueiras <>
2018-10-16 00:38:35 +0100

    Created separated config file to store critical authentication data

--- src/app/core/services/authentication.service.ts
+++ src/app/core/services/authentication.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 
 import Auth0Cordova from '@auth0/cordova';
 
+const authenticationSettings = require('../../../authentication-settings.json');
+
 @Injectable({
     providedIn: 'root',
 })
@@ -16,15 +18,14 @@ export class AuthenticationService {
         // https://github.com/auth0/auth0-cordova
 
         const auth0 = new Auth0Cordova({
-            domain: 'stg-identity.primaverabss.com/connect', // '{YOUR_AUTH0_DOMAIN}',
-            clientId: '',
-            packageIdentifier: 'com.primavera.v10', //  '{WIDGET_ID_IN_CONFIG_XML}'
+            domain: 'stg-identity.primaverabss.com/connect',
+            clientId: authenticationSettings.clientId,
+            packageIdentifier: 'com.primavera.v10',
         });
 
         const options = {
-            scope: 'openid profile email lithium-mobile offline_access',
-            responseType: 'code token',
-            redirectUri: 'com.primavera.v10://' // 'primaverav10mobile://'
+            scope: 'openid profile email lithium-mobile',
+            responseType: 'code token'
         };
 
         auth0.authorize(options, function (err, result) {
