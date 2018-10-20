d8eb69f04e4303a4baef2b440ab14aa485481f37
António Felgueiras <>
2018-10-20 19:07:24 +0100

    Created Instances Service

--- src/app/core/services/authentication.service.ts
+++ src/app/core/services/authentication.service.ts
@@ -12,7 +12,9 @@ export class AuthenticationService {
     authenticate() {
         // Redirect back to app after authenticating
         (window as any).handleOpenURL = (url: string) => {
-            Auth0Cordova.onRedirectUri(url);
+            // replace the spaces on url with the space code '%20';
+            const finalUrl = url.split(' ').join('%20');
+            Auth0Cordova.onRedirectUri(finalUrl);
         };
 
         // https://github.com/auth0/auth0-cordova
@@ -24,8 +26,9 @@ export class AuthenticationService {
         });
 
         const options = {
-            scope: 'openid profile email lithium-mobile',
-            responseType: 'code token'
+            scope: 'openid profile email lithium-mobile offline_access',
+            responseType: 'code token',
+            redirectUri: 'com.primavera.v10://'
         };
 
         auth0.authorize(options, function (err, result) {
