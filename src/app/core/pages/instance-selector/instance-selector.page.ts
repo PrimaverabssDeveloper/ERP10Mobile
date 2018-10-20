3c3fabb4fcb05c01a941d4ddb792a91436855ab9
António Felgueiras <>
2018-10-20 19:04:08 +0100

    Created Instance Selector Page

---
+++ src/app/core/pages/instance-selector/instance-selector.page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    templateUrl: 'instance-selector.page.html',
+    styleUrls: ['instance-selector.page.scss'],
+})
+export class InstanceSelectorPage {
+
+    constructor() {
+    }
+}
