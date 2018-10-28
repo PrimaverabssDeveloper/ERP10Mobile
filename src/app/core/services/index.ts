42659f4830d088d5c3cb60f4843e92238b87a07c
António Felgueiras <>
2018-10-28 03:06:11 +0000

    Added storage service to core services

--- src/app/core/services/index.ts
+++ src/app/core/services/index.ts
@@ -2,15 +2,18 @@ import { AuthenticationService } from './authentication.service';
 import { InstancesService } from './instances.service';
 import { HttpRequestService } from './http-request.service';
 import { InstanceHttpRequestService } from './instance-http-requests.service';
+import { CoreStorageService } from './core-storage.service';
 
 export * from './authentication.service';
 export * from './instances.service';
 export * from './http-request.service';
 export * from './instance-http-requests.service';
+export * from './core-storage.service';
 
 export const SERVICES = [
     AuthenticationService,
     InstancesService,
     HttpRequestService,
     InstanceHttpRequestService,
+    CoreStorageService
 ];
