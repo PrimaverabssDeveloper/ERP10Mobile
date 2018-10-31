d25b280cc8e2371e98cb47f39235f137b8c17e8d
António Felgueiras <>
2018-10-31 01:03:39 +0000

    Implemented dynamic tickers.

--- src/app/shell/components/tickers/tickers.component.ts
+++ src/app/shell/components/tickers/tickers.component.ts
@@ -1,11 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, Input, ViewChild, OnInit } from '@angular/core';
+import { ModuleSummary } from '../../entities';
+import { Slides } from '@ionic/angular';
 
 
+/**
+ * This component is the slider that shows the summaries for the modules that have summary data.
+ *
+ * @export
+ * @class TickersComponent
+ */
 @Component({
     selector: 'tickers',
     templateUrl: './tickers.component.html',
     styleUrls: ['./tickers.component.scss']
 })
-export class TickersComponent {
+export class TickersComponent implements OnInit {
+    @Input() tickers: HTMLElement[];
 
+    @ViewChild('slide') slide: Slides;
+
+    constructor() {
+
+    }
+
+    /**
+    * Execute on page initialization.
+    *
+    * @memberof TickersComponent
+    */
+    ngOnInit(): void {
+        if (this.slide) {
+            setTimeout(() => {
+                this.slide.update();
+            }, 1000);
+        }
+    }
 }
