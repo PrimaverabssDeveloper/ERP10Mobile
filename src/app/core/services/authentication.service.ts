345a12a13b05e8b8477bbb3d643b88345fbb321a
António Felgueiras <>
2018-10-31 00:56:07 +0000

    enabled authentication

--- src/app/core/services/authentication.service.ts
+++ src/app/core/services/authentication.service.ts
@@ -16,36 +16,41 @@ const authenticationSettings = require('../../../authentication-settings.json');
 })
 export class AuthenticationService {
 
+    constructor(private http: HttpClient) {
+    }
+
     authenticate() {
 
-        // // Redirect back to app after authenticating
-        // (window as any).handleOpenURL = (url: string) => {
-        //     // replace the spaces on url with the space code '%20';
-        //     const finalUrl = url.split(' ').join('%20');
+        return;
 
-        //     Auth0Cordova.onRedirectUri(finalUrl);
-        // };
+        // Redirect back to app after authenticating
+        (window as any).handleOpenURL = (url: string) => {
+            // replace the spaces on url with the space code '%20';
+            const finalUrl = url.split(' ').join('%20');
 
-        // // https://github.com/auth0/auth0-cordova
+            Auth0Cordova.onRedirectUri(finalUrl);
+        };
 
-        // const auth0 = new Auth0Cordova({
-        //     domain: 'stg-identity.primaverabss.com/connect',
-        //     clientId: authenticationSettings.clientId,
-        //     packageIdentifier: 'com.primavera.v10',
-        // });
+        // https://github.com/auth0/auth0-cordova
 
-        // const options = {
-        //     scope: 'openid profile email lithium-mobile offline_access',
-        //     responseType: 'code token',
-        //     redirectUri: 'com.primavera.v10://'
-        // };
+        const auth0 = new Auth0Cordova({
+            domain: 'stg-identity.primaverabss.com/connect',
+            clientId: authenticationSettings.clientId,
+            packageIdentifier: 'com.primavera.v10',
+        });
 
-        // auth0.authorize(options, function (err, result) {
-        //     if (err) {
-        //         console.log('Error authenticating');
-        //     } else {
-        //         console.log('Authentication with success');
-        //     }
-        // });
+        const options = {
+            scope: 'openid profile email lithium-mobile offline_access',
+            responseType: 'code token',
+            redirectUri: 'com.primavera.v10://'
+        };
+
+        auth0.authorize(options, function (err, result) {
+            if (err) {
+                console.log('Error authenticating');
+            } else {
+                console.log('Authentication with success');
+            }
+        });
     }
 }
