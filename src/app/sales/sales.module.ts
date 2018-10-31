e859bf09cf75938270f4eb39a8dc8bf283804416
António Felgueiras <>
2018-10-31 01:04:06 +0000

    Implemented Sales Summary Handler.

--- src/app/sales/sales.module.ts
+++ src/app/sales/sales.module.ts
@@ -2,23 +2,29 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
-
 
 import { PAGES } from './pages';
 import { SALES_ROUTING } from './sales.routes';
 import { ENTRY_COMPONENTS } from './components';
-import { SERVICES } from './services';
+import { SERVICES, SalesService } from './services';
+import { ModulesSummariesService } from '../core/services';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    SALES_ROUTING
-  ],
-  declarations: [PAGES, ENTRY_COMPONENTS],
-  entryComponents: [ENTRY_COMPONENTS],
-  providers: [SERVICES]
+    imports: [
+        CommonModule,
+        FormsModule,
+        IonicModule,
+        SALES_ROUTING
+    ],
+    declarations: [PAGES, ENTRY_COMPONENTS],
+    entryComponents: [ENTRY_COMPONENTS],
+    providers: [SERVICES]
 })
-export class SalesModule {}
+export class SalesModule {
+    constructor(
+        private modulesSummariesService: ModulesSummariesService,
+        private salesService: SalesService
+    ) {
+        this.modulesSummariesService.addModuleSummariesHandler('sales', () => this.salesService.createSalesSummaryTickers());
+    }
+}
