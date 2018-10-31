79f7653627651371285fa9613b98dc77d18e9072
António Felgueiras <>
2018-10-31 01:02:16 +0000

    New entity ModuleSummary to support the Modules Summary logic

---
+++ src/app/shell/entities/module-summary.ts
@@ -0,0 +1,12 @@
+import { Module } from '../../core/entities';
+
+/**
+ * Contains de summary data for a module.
+ *
+ * @export
+ * @interface ModuleSummary
+ */
+export interface ModuleSummary {
+    module: Module;
+    data: any;
+}
