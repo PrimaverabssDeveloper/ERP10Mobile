f3f811d149624caaa0e6e86ff9a4248e57cc540f
António Felgueiras <>
2018-10-31 00:57:23 +0000

    Added two new services references: DomService and ModulesSumariesService

--- src/app/core/services/index.ts
+++ src/app/core/services/index.ts
@@ -3,17 +3,23 @@ import { InstancesService } from './instances.service';
 import { HttpRequestService } from './http-request.service';
 import { InstanceHttpRequestService } from './instance-http-requests.service';
 import { CoreStorageService } from './core-storage.service';
+import { DomService } from './dom.service';
+import { ModulesSummariesService } from './modules-summaries.service';
 
 export * from './authentication.service';
 export * from './instances.service';
 export * from './http-request.service';
 export * from './instance-http-requests.service';
 export * from './core-storage.service';
+export * from './dom.service';
+export * from './modules-summaries.service';
 
 export const SERVICES = [
     AuthenticationService,
     InstancesService,
     HttpRequestService,
     InstanceHttpRequestService,
-    CoreStorageService
+    CoreStorageService,
+    DomService,
+    ModulesSummariesService
 ];
