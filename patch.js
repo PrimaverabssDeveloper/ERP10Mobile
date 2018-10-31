54d1830de14ae07ba9a5b30c2d4c38cbb4eeeccf
António Felgueiras <>
2018-10-31 00:55:48 +0000

    Added process to remove 'oauth' from the auth0.min.esm.js so it performs the right requests to the authentication server

--- patch.js
+++ patch.js
@@ -1,8 +1,9 @@
 // https://github.com/angular/angular-cli/issues/1548#issuecomment-409086413
 
 const fs = require('fs');
-const f = 'node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/webpack-configs/browser.js';
 
+// add crypto lib to node
+const f = 'node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/webpack-configs/browser.js';
 fs.readFile(f, 'utf8', function (err,data) {
   if (err) {
     return console.log(err);
@@ -10,6 +11,21 @@ fs.readFile(f, 'utf8', function (err,data) {
   var result = data.replace(/node: false/g, 'node: {crypto: true, stream: true}');
 
   fs.writeFile(f, result, 'utf8', function (err) {
+    if (err) return console.log(err);
+  });
+});
+
+
+// fix oauth request token endpoint
+const auth0LibPath = 'node_modules/auth0-js/dist/auth0.min.esm.js';
+fs.readFile(auth0LibPath, 'utf8', function (err,data) {
+  if (err) {
+    return console.log(err);
+  }
+
+  var result = data.replace(/,"oauth"/g, '');
+
+  fs.writeFile(auth0LibPath, result, 'utf8', function (err) {
     if (err) return console.log(err);
   });
 });
