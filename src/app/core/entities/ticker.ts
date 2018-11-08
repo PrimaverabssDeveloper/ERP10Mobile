e56a670287306d9cb33e165167e7db7c54ce078b
António Felgueiras <>
2018-11-08 00:52:59 +0000

    Created class to house the ticker content and title

---
+++ src/app/core/entities/ticker.ts
@@ -0,0 +1,24 @@
+
+/**
+ * Defines a summary ticker.
+ *
+ * @export
+ * @interface Ticker
+ */
+export interface Ticker {
+    /**
+     * The ticker title.
+     *
+     * @type {string}
+     * @memberof Ticker
+     */
+    title: string;
+
+    /**
+     * The ticker html content.
+     *
+     * @type {HTMLElement}
+     * @memberof Ticker
+     */
+    content: HTMLElement;
+}
