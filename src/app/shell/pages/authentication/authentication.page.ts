f23268f45997039d0ba417e42261f17e8f63a251
António Felgueiras <>
2018-11-13 04:08:44 +0000

    Now if the app is running on the webbrowser, the login button will authenticate in demo mode

--- src/app/shell/pages/authentication/authentication.page.ts
+++ src/app/shell/pages/authentication/authentication.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthenticationService } from '../../../core/services';
 import { Router } from '@angular/router';
+import { AppSettings } from '../../../core/app-settings';
+import { AlertController } from '@ionic/angular';
 
 @Component({
     templateUrl: './authentication.page.html',
@@ -11,7 +13,9 @@ export class AuthenticationPage implements OnInit {
 
     constructor(
         private authenticationService: AuthenticationService,
-        private router: Router
+        private router: Router,
+        private appSettings: AppSettings,
+        private alertController: AlertController
     ) {
     }
 
@@ -29,9 +33,25 @@ export class AuthenticationPage implements OnInit {
     }
 
     async loginAction() {
-        const isAuthenticated = await this.authenticationService.authenticate();
-        if (isAuthenticated) {
-            this.router.navigate(['/shell/instances']);
+
+        if (this.appSettings.isMobilePlatform) {
+            const isAuthenticated = await this.authenticationService.authenticate();
+            if (isAuthenticated) {
+                this.router.navigate(['/shell/instances']);
+            }
+        } else {
+            const alert = await this.alertController.create({
+                header: 'Authentication',
+                message: 'The authentication can only be performed when the app is running on a mobile device. Starting in demo.',
+                buttons: [{
+                    text: 'ok',
+                    handler: () => {
+                        this.demoAction();
+                    }
+                }]
+            });
+
+            await alert.present();
         }
     }
 
