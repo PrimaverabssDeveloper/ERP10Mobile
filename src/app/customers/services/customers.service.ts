8b1b8bb2925086e8344cbcfbc6bfd7f3d0f9f473
António Felgueiras <>
2018-11-13 04:17:49 +0000

    Implemented methods to save and read the recent viewed customers

--- src/app/customers/services/customers.service.ts
+++ src/app/customers/services/customers.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@angular/core';
 import { CustomersSearchResult, Customer } from '../entities';
+import { CustomersStorageService } from './customers-storage.service';
 
 @Injectable()
 export class CustomersService {
+
+    private static readonly RECENT_VIEWED_CUSTOMERS_STORAGE_KEY = 'RECENT_VIEWD_CUSTOMERS_STORAGE_KEY';
+
+    constructor(protected storageService: CustomersStorageService) {
+    }
 
     searchCustomers(searchTerm: string): Promise<CustomersSearchResult> {
         return new Promise<CustomersSearchResult>(result => {
@@ -14,5 +20,32 @@ export class CustomersService {
         return new Promise<Customer>(result => {
             result(null);
         });
+    }
+
+    async getRecentViewedCustomers(): Promise<Customer[]> {
+        const result = await this.storageService
+                                 .getData<Customer[]>(CustomersService.RECENT_VIEWED_CUSTOMERS_STORAGE_KEY, true);
+        if (result) {
+            return result;
+        } else {
+            return [];
+        }
+    }
+
+    async addToRecentViewedCustomers(customer: Customer): Promise<any> {
+        const recentViewedCustomers = await this.getRecentViewedCustomers();
+        const index = recentViewedCustomers.findIndex(
+            c => c.key === customer.key && c.companyKey === customer.companyKey
+        );
+
+        // this customer already exist
+        // lets remove it, otherwise it will be duplicated
+        if (index >= 0) {
+            recentViewedCustomers.splice(index, 1);
+        }
+
+        recentViewedCustomers.unshift(customer);
+
+        await this.storageService.setData(CustomersService.RECENT_VIEWED_CUSTOMERS_STORAGE_KEY, recentViewedCustomers, true);
     }
 }
