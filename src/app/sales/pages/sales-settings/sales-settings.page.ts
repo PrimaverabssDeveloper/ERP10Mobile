6a982f0ab72e294d1b6248253e73eb027e960b09
António Felgueiras <amgfelgueiras@gmail.com>
2018-12-28 11:29:35 +0000

    Created Sales Settings page

--- src/app/sales/pages/sales-settings/sales-settings.page.ts
+++ src/app/sales/pages/sales-settings/sales-settings.page.ts
@@ -4,15 +4,60 @@ import { LoadingController } from '@ionic/angular';
 import { PageBase } from '../../../shared/pages';
 import { SalesService, SalesServiceProvider } from '../../services';
 
+/**
+ * Settings for the sales module.
+ *
+ * @export
+ * @class SalesSettingsPage
+ * @extends {PageBase}
+ * @implements {OnInit}
+ */
 @Component({
     templateUrl: 'sales-settings.page.html',
-    styleUrls: ['sales-settings.page.scss'],
+    styleUrls: [
+        'sales-settings.page.scss',
+        '../../../shared/styles/settings.scss'
+    ],
     providers: [SalesServiceProvider]
 })
 export class SalesSettingsPage extends PageBase implements OnInit {
 
+    // #region 'Private Properties'
+    _useReferenceCurrency: boolean;
+    _showAggregateData: boolean;
+    _showDailySales: boolean;
+    // #endregion
+
+
+    // #region 'Public Properties'
+    get useReferenceCurrency(): boolean {
+        return this._useReferenceCurrency;
+    }
+
+    set useReferenceCurrency(value: boolean) {
+        this._useReferenceCurrency = value;
+    }
+
+    get showAggregateData(): boolean {
+        return this._showAggregateData;
+    }
+
+    set showAggregateData(value: boolean) {
+        this._showAggregateData = value;
+    }
+
+    get showDailySales(): boolean {
+        return this._showDailySales;
+    }
+
+    set showDailySales(value: boolean) {
+        this._showDailySales = value;
+    }
+    // #endregion
+
     constructor(
         public loadingController: LoadingController,
+        private salesService: SalesService
     ) {
         super(loadingController);
     }
