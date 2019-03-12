c40311c91a9ca947d9fa35441bf79325864d3eb9
António Felgueiras <amgfelgueiras@gmail.com>
2019-03-12 00:47:35 +0000

    Added server endpoint to HR

---
+++ src/app/human-resources/module-definition.ts
@@ -0,0 +1,16 @@
+import { ModuleDefinition } from '../core/entities';
+
+export const HrModuleDefinition: ModuleDefinition = {
+    key: 'HumanResources',
+    displayRelevance: 1,
+    moduleRoutePath: '/humanresources',
+    iconPath: '/assets/human-resources/human_resources_logo.svg',
+    localizedNameKey: 'HUMAN_RESOURCES.MODULE_NAME',
+    summaries: {
+        hasSummaries: false
+    },
+    settings: {
+        hasSettings: true,
+        settingsRoutePath: ''
+    }
+};
